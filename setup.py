92437e8ab3022644cc0436caf73843781b4ea6c6
Jack Kelly <jack-list@xlk.org.uk>
2013-01-22 16:50:56 +0000

    just testing updating procedure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cosmSender",
-    version = "0.11",
+    version = "0.12",
     packages = find_packages(),
     install_requires = ['pyserial>=2.4', 'simplejson'],
     author = "Jack Kelly",
